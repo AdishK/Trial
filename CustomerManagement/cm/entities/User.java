4b54db6c8e23f7e5b87c67cea37676a071bf1010
afour-adishk <adish.katkar@afourtech.com>
2015-05-23 20:16:55 +0530

    COmmiting User POJO

---
+++ CustomerManagement/cm/entities/User.java
@@ -0,0 +1,13 @@
+
+public class User {
+
+	private String userName;
+	private String emailId;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String gender;
+	private String phoneNumber;
+	private String address;
+	
+}
